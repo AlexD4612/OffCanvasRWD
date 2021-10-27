9a17934307628b0df42b2619e14ecd1a40a230ee
Alex <Adavisson@eagles.ewu.edu>
2021-10-27 03:45:56 -0700

    media queries wokring, skeleton with no data made

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-// Check-Check
-console.log("👍️");
-
 // Get Elements
 const menuButton = document.querySelector("a.toggle-nav");
 const mainContent = document.querySelector("main");
 
 // Listen for clicks on `menuButton`
-menuButton.addEventListener("click", function () {
-  // Toggle classes on `mainContent`
-  mainContent.classList.toggle("open");
+menuButton.addEventListener("click", function() {
+    // Toggle classes on `mainContent`
+    mainContent.classList.toggle("open");
 
-  // If opened, change change text
-  if (mainContent.classList.contains("open")) {
-    menuButton.innerHTML = `<img src="assets/close.svg" /> Close`;
-  } else {
-    menuButton.innerHTML = `<img src="assets/menu.svg" /> Menu`;
-  }
+    // If opened, change change text
+    if (mainContent.classList.contains("open")) {
+        menuButton.innerHTML = `<img src="assets/close.svg" /> Close`;
+    } else {
+        menuButton.innerHTML = `<img src="assets/menu.svg" /> Menu`;
+    }
 });
+
+//Date info
+const curDate = new Date();
+const year = curDate.getFullYear();
+const yearH = document.querySelector("span.year");
+yearH.innerHTML = year;
+
+
+//Media Queries
+const subtitle = document.querySelector("h2");
+const holiday = document.querySelector("span.holiday");
+const device = document.querySelector("span.device");
+var mobile = window.matchMedia("screen and (min-width:0) and (max-width:26em)");
+var tablet = window.matchMedia("screen and (min-width: 26em) and (max-width:72em)");
+var desktop = window.matchMedia("screen and (min-width: 72em)");
+
+
+function mobileQ(mobile) {
+    if (mobile.matches) {
+        subtitle.innerHTML = "Christmas";
+        device.innerHTML= "Mobile";
+        holiday.innerHTML = "Christmas";
+       
+
+    }
+}
+
+function tabletQ(tablet) {
+    if (tablet.matches) {
+        subtitle.innerHTML = "Thanksgiving";
+        device.innerHTML= "Tablet";
+        holiday.innerHTML = "Thanksgiving";
+
+    }
+}
+
+function desktopQ(desktop) {
+    if (desktop.matches) {
+        subtitle.innerHTML = "Halloween";
+        device.innerHTML= "Desktop";
+        holiday.innerHTML = "Halloween";
+
+    }
+}
+
+
+mobileQ(mobile);
+tabletQ(tablet);
+desktopQ(desktop);
+mobile.addListener(mobileQ);
+tablet.addListener(tabletQ);
+desktop.addListener(desktopQ);
